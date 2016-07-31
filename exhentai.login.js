1bedaa9054a19cacd74fc11d0306aad1549cce5b
qw4wer <qw5wer@gmail.com>
2016-07-31 19:03:03 +0800

    Update exhentai.login.js

--- exhentai.login.js
+++ exhentai.login.js
@@ -1,18 +1,15 @@
 // ==UserScript==
-// @name           {Userscript name}
+// @name           exhentai_login
 // @namespace      {e.g. your website}
-// @author         {your name}
+// @author         qw4wer
 // @description    {Userscript description}
-// @include        exhentai.org/*
-//		https://developer.mozilla.org/en-US/Add-ons/SDK/Low-Level_APIs/util_match-pattern
-//		
+// @include exhentai.org/*
 // @clean-include       (true || false)
 // if it's > true < , USI will not change any of your @includes
-// 
 // @info                {more informations ...}
 // @updateURL           {USI can update your Userscript, if the version number has changed}
 // @run-at              (document-end || document-start || document-ready)
-// @include-jquery      (true || false)
+// @include-jquery      true
 // @use-greasemonkey    (true || false)
 // @version             1.0.0
 // ==/UserScript==
