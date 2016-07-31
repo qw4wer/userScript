3fce4078dce29ca87cecf7bf4d67624c28ce2c90
qw4wer <qw5wer@gmail.com>
2016-07-31 18:42:35 +0800

    Create exhentai.login.js

---
+++ exhentai.login.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name           exhentai_login
+// @namespace      {e.g. your website}
+// @author         qw4wer
+// @description    {Userscript description}
+// @include         https://exhentai.org
+//		
+// @clean-include       (true || false)
+// if it's > true < , USI will not change any of your @includes
+// 
+// @info                {more informations ...}
+// @updateURL           {USI can update your Userscript, if the version number has changed}
+// @run-at              (document-end || document-start || document-ready)
+// @include-jquery      true
+// @use-greasemonkey    (true || false)
+// @version             1.0.0
+// ==/UserScript==
+
+// WARNING: If you save this Userscript, it will be immediately active
+
+document.addEventListener("click", function(){
+	alert("You have clicked!");
+});
