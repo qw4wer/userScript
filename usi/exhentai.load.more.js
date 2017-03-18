7947e1dff6d24f3bafeaa38312f99d0bb9259fde
qw4wer <qw5wer@gmail.com>
2017-03-19 00:06:57 +0800

    去除错误加载后刷新页面
    去除错误加载后刷新页面

--- usi/exhentai.load.more.js
+++ usi/exhentai.load.more.js
@@ -20,7 +20,6 @@
 // ==/UserScript==
 
 
-$ = $ || unsafeWindow.$;
 document = document || unsafeWindow.document;
 (function () {
     var isOther = location.href.indexOf('https://exhentai.org/s') == -1;
@@ -28,7 +27,7 @@ document = document || unsafeWindow.document;
     if (!isOther) {
         setTimeout(function () {
             init();
-            loadTools();
+           
         }, 2000);
 
     } else {
@@ -39,7 +38,7 @@ document = document || unsafeWindow.document;
 
 function loadTools() {
     $("#i1 h1").append("<input type='button' onclick='method(this)' value='加载'/>");
-    loadJs(method.toString()+reloadImg.toString());
+    loadJs(method.toString()+reloadImg.toString()+nl.toString() +onErrorReload.toString());
 }
 
 function reloadImg(img){
@@ -48,8 +47,15 @@ function reloadImg(img){
     $(img).next().attr("src",$(img).next().attr("src")+"?t="+Date.parse(new Date()));
 }
 
+function onErrorReload(event){
+$(event.target).attr('src',$(event.target).attr("src")+"?t="+Date.parse(new Date()));
+}
+
+function nl(a) {
+return false;
+}
+
 function method(btn) {
-    debugger;
     $(btn).attr("disabled","disabled");
     
     $("#i1").css({width:'auto',maxWidth:''});
@@ -60,12 +66,57 @@ function method(btn) {
 
     var nextPage = $("#i2 div a:eq(2)").attr("href");
 
-    var first = $("#i3 img");
+    var first = $("#i3 img").removeAttr("onerror");
 
     $("#i3 a").remove();
 
     $("#i3").append(div.clone().append(first));
 
+    var arr = [];
+    loadImage(nextPage,arr);
+    function loadImage(url,arr){
+        $.ajax({
+            url:url,
+            dataType:'html',
+            async:true,
+            success:function(data){
+                arr.push($("#i3 img",data));
+                $("#i3 div:last").after(div.clone().append($("#i3 img",data).removeAttr("onerror").attr('onerror',"onErrorReload(this)")));
+                var next = $("#i2 div a:eq(2)",data).attr("href");
+                var page = next.substring(next.indexOf("-")+1);
+                if(parseInt(page)  < parseInt(max))
+                    setTimeout(loadImage(next,arr),1500);
+
+            }
+
+        });
+    }
+}
+
+
+
+function init() {
+    GM_xmlhttpRequest({
+        method: "get",
+        url: 'http://code.jquery.com/jquery-2.1.1.min.js',
+        synchronous: false,
+        onload: function (response) {
+            loadJs(response.responseText);
+             loadTools();
+        }
+    });
+
+}
+
+
+// 工具方法  //
+function loadJs(jsStr) {
+    var oHead = document.getElementsByTagName('HEAD')[0],
+        oScript = document.createElement('script');
+    oScript.type = 'text/javascript';
+    oScript.text = jsStr;
+    oHead.appendChild(oScript);
+}
     var arr = [];
     loadImage(nextPage,arr);
     function loadImage(url,arr){
